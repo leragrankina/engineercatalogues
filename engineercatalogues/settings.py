685b04ab856abf97a6e049faf9149c4e27897ce9
Lera Grankina <leragrankina@gmail.com>
2016-12-11 14:12:49 +0200

    Login and logout redirect to previous page

--- engineercatalogues/settings.py
+++ engineercatalogues/settings.py
@@ -132,8 +132,6 @@ STATICFILES_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'index'
 ACCOUNT_ACTIVATION_DAYS = 7
 
 EMAIL_USE_TLS = True
