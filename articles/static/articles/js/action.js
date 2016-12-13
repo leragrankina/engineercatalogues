66504fdfe4c5a8dc7fb2ace076b5fe242cf8faa2
Lera Grankina <leragrankina@gmail.com>
2016-12-13 10:45:38 +0200

    Comment submits and updates right

--- articles/static/articles/js/action.js
+++ articles/static/articles/js/action.js
@@ -1,3 +1,37 @@
 /**
  * Created by Valeriia on 13.12.2016.
  */
+$(document).ready(function(){
+    $('.btn-crud').hide();
+    $('.comment-col .form-crud').hide();
+
+    $('#comment_input').on('click', function(){
+        $(this).height(150);
+        $(this).attr('placeholder', '');
+});
+
+    $('.comment-col').on('mouseenter', function () {
+        btns = $(this).find('.btn-crud');
+        btns.show();
+        comment_col = $(this);
+        btns.first().on('click', function () {
+            comment_col.find('.form-crud:last-child').submit();
+        });
+        btns.last().on('click', function(){
+            comment_text = comment_col.find('.comment-text').hide();
+            comment_update = comment_col.find('.form-crud').first();
+            comment_update.find('button:last-child').on('click', function(){
+                comment_update.find('textarea').val(comment_text.text());
+                comment_update.hide();
+                comment_text.show();
+            });
+            comment_update.show();
+        });
+
+    });
+
+    $('.comment-col').on('mouseleave', function(){
+        $(this).find('.btn-crud').hide();
+    });
+
+});
