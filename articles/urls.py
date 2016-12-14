2a7d46007e9856b6d0a3463e7a6b0e7d2da50cca
Lera Grankina <leragrankina@gmail.com>
2016-12-14 09:49:01 +0200

    Dropdown menu done right.

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from .views import ArticleList, ArticleDetail, save_comment, CommentDelete, update_comment
+from .views import ArticleList, ArticleDetail, save_comment, CommentDelete, update_comment, signin
 
 urlpatterns = [
     url(r'^$', ArticleList.as_view(), name='list'),
     url(r'^(?P<pk>[0-9]+)$', ArticleDetail.as_view(), name="detail"),
     url(r'^(?P<pk>[0-9]+)/comment$', save_comment, name='add_comment'),
     url(r'^comment/(?P<pk>[0-9]+)/delete$', CommentDelete.as_view(), name='delete_comment'),
-    url(r'^comment/(?P<pk>[0-9]+)/update', update_comment, name='update_comment')
+    url(r'^comment/(?P<pk>[0-9]+)/update', update_comment, name='update_comment'),
+    url(r'^signin$', signin, name='signin')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, Http404, HttpResponse, get_object_or_404
-from django.views.generic import ListView, DetailView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, TemplateView
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth import authenticate, login
 
 from .models import Article, Comment
 
@@ -48,3 +49,15 @@ def update_comment(request, pk):
         comment.text = comment_text
         comment.save()
     return HttpResponseRedirect(reverse_lazy('articles:detail', args=(comment.article.pk,)))
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', False)
+        password = request.POST.get('password', False)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(request.GET['next'])
+        return render(request, 'registration/login.html', {'error': 'Неправильный логин или пароль'})
+
